1132e91ae4e7198c344a4c02968b6a2af2d7a4fb
Kerry Smith <kerrysfs@gmail.com>
2020-08-29 19:52:21 -0700

    fixed script loading fist, and changed some styling. I am in the process of coding this out with more features and in react

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_", "'"]
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let sym = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_", "'"]
 
-var userLength = parseInt(prompt("How long do you want your password? Enter between 8-128 characters"));
+let userLength = parseInt(prompt("How long do you want your password? Enter between 8-128 characters"));
 
 while (userLength < 8 || userLength > 128 || isNaN(userLength)) {
     userLength = parseInt(prompt("How long do you want your password? Enter between 8-128 characters"));
 }
 
-var userSpecial = confirm("Would you like special characters in your password?");
-var userNum = confirm("Would you like numbers in your password?");
-var userUpper = confirm("Would you like upper case letters in your password?");
-var userLower = confirm("Would you like lower case letters in your password?");
-
-
-
-
-
-
-
+let userSpecial = confirm("Would you like special characters in your password?");
+let userNum = confirm("Would you like numbers in your password?");
+let userUpper = confirm("Would you like upper case letters in your password?");
+let userLower = confirm("Would you like lower case letters in your password?");
 
 
 function randomGen(array) {
     // return a random chara from lower set
     return array[Math.floor(Math.random() * array.length)];
-
 }
-
 
 function generatePassword() {
 
-    var password = "";
-    for (var i = 0; i < userLength; i++) {
+    let password = "";
+    for (let i = 0; i < userLength; i++) {
         if (userLower && password.length !== userLength) {
             password += randomGen(lowerCase)
         }
@@ -47,96 +38,19 @@ function generatePassword() {
         }
     }
     return password
-
 }
 
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 // var generatePassword = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// generatePassword.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var userChoice =
-
-
-
-
-
-
-
-
-
-
-    // function to get random number between min and max value //
-    // function getRandomInt(min, max) {
-    //     return Math.floor(Math.random() * (max - min + 1)) + min;
-    //     console.log(9, 120);
-    // }
-
-
-
-
-// var keylist = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+{}./][?"
-// var temp = "";
-
-// function generatePassword(plength) {
-//     temp = "";
-//     for (i = 0; i < plength; i++)
-//         temp += keylist.charAt(Math.floor(Math.random() * keylist.length))
-//     return temp
-// }
-
-// function populateForm(enterLength) {
-//     document.pgenerate.output.value = gen
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var questions = [
-//     {q:"Would you like special characters in your password?", a: true},
-//     {q:"Would you like numbers in your password?", a: true},
-//     {q:"Would you like lower case letters in your password?", a: true},
-//     {q:"Would you like to include upper case letters too?", a: true}
-// ];
+// generatePassword.addEventListener("click", writePassword)
